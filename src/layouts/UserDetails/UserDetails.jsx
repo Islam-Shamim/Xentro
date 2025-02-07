0d61f60ec35c0cff47c03a5c2f7fbbfb7fb27ecf
Islam-Shamim <islamshamim1819@gmail.com>
2025-02-07 23:29:00 +0600

    details

--- src/layouts/UserDetails/UserDetails.jsx
+++ src/layouts/UserDetails/UserDetails.jsx
@@ -3,7 +3,7 @@ import { useLoaderData, useNavigate } from "react-router-dom";
 const UserDetails = () => {
     const navigate = useNavigate();
     const details = useLoaderData();
-    const { name, email } = details;
+    const { name, email, phone,website } = details;
 
     const handleGoBack = () => {
         navigate(-1);
@@ -13,8 +13,10 @@ const UserDetails = () => {
         <div className="container mx-auto mt-10">
             <div className="card bg-gray-500 text-white w-96">
                 <div className="card-body">
-                    <h2 className="card-title">{name}</h2>
-                    <p>{email}</p>
+                    <h2 className="card-title">Name : {name}</h2>
+                    <p>Email: {email}</p>
+                    <p>Phone: {phone}</p>
+                    <p>Website: {website}</p>
                 </div>
                 <div className="m-4 px-2">
                         <button className="btn btn-primary" onClick={handleGoBack}>Go back</button>
